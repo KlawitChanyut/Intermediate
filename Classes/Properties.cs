772b9c05de57897bc1a41342be1b19a7cf58eab3
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 16:03:10 +0700

    Update get

--- Classes/Properties.cs
+++ Classes/Properties.cs
@@ -9,5 +9,16 @@ namespace Intermediate.Classes
     class Properties
     {
         public DateTime Birthdate { get; set; }
+
+        public int Age
+        {
+            get 
+            { 
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
     }
 }
