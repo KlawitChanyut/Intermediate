b71670f81b813c552dfcb8e4caaeef6d877e6d55
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 05:14:55 +0700

    Create class Interface_and_Polymorphism

---
+++ Interfaces/Interface and Polymorphism.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Interfaces
+{
+    public class Interface_and_Polymorphism
+    {
+        private readonly IList<INotificationChannel> _notificationChannels;
+
+        public VideoEncoder()
+        {
+            _notificationChannels = new List<INotificationChannel>();
+        }
+    }
+}
