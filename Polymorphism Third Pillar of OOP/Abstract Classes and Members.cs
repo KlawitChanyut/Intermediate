eeb54ba90b4e2ad0dffe1c1fe04dd9d6704e947a
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 03:42:58 +0700

    Create class Abstract_Classes_and_Members

---
+++ Polymorphism Third Pillar of OOP/Abstract Classes and Members.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Polymorphism_Third
+{
+    public class Abstract_Classes_and_Members
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
