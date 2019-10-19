11d357b336c8aff8b1d7e5dde3900629b8a1af2a
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 05:09:08 +0700

    Create class Interfaces_are_NOT

---
+++ Interfaces/Interfaces are NOT.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Interfaces
+{
+    public class TextBox : Interfaces_are_NOT, IDraggable, IDroppable
+    {
+        public void Drag()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Interfaces_are_NOT
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public Position TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+
+        public void Focus()
+        {
+            Console.WriteLine("Received focus.");
+        }
+    }
+}
