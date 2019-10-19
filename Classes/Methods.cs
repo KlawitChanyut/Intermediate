b065230f3fec9df9a28b477dd46a326d740d8970
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 14:57:53 +0700

    Create class Methods

---
+++ Classes/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Classes
+{
+    public class Methods
+    {
+        public int X;
+        public int Y;
+
+        public Methods(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
