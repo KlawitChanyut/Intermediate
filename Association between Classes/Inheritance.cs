91bca8f13071e90d273424b73a967d72ba06efa3
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 16:29:28 +0700

    Create Inheritance

---
+++ Association between Classes/Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Association_between_Classes
+{
+    public class Inheritance
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+
+        }
+
+        public void Duplicate()
+        {
+
+        }
+    }
+}
