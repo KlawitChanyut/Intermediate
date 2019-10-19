bd1fa64f1052e2ec2467712ccd1e4a09aadc971b
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 03:35:07 +0700

    Update class Triangle : Shape

--- Polymorphism Third Pillar of OOP/Method Overriding.cs
+++ Polymorphism Third Pillar of OOP/Method Overriding.cs
@@ -21,4 +21,25 @@ namespace Intermediate.Polymorphism_Third
             Console.WriteLine("Draw a rectangle");
         }
     }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a triangle");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+
 }
