18119fddd51a386e42804d19a8acf493de9d4d82
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 02:48:57 +0700

    Create class Boxing_and_Unboxing

---
+++ Inheritance - Second Pillar of OOP/Boxing and Unboxing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Inheritance_Second
+{
+    public class Boxing_and_Unboxing
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(DateTime.Today);
+        }
+    }
+}
