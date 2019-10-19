fa4df8e2b28ef1313c004a3fc4dfa4c2e18f893c
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 14:05:45 +0700

    Create Introduction to Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
