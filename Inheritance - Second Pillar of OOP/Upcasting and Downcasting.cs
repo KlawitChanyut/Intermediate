3b8e02b8d82890591132ab312da877f4fa9a0ca8
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 02:32:12 +0700

    Create class Upcasting_and_Downcasting

---
+++ Inheritance - Second Pillar of OOP/Upcasting and Downcasting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Inheritance_Second
+{
+    public class Upcasting_and_Downcasting
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Draw()
+        {
+
+        }
+    }
+
+    public class Tesxt : Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
