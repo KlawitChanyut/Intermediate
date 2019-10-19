944870875c434056e228ca63e0c8b54910d9ee81
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 15:53:04 +0700

    Update GetBirthdate

--- Classes/Access Modifiers.cs
+++ Classes/Access Modifiers.cs
@@ -8,9 +8,26 @@ namespace Intermediate.Classes
 {
     public class Person
     {
-        private DateTime _birtdate;
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
     }
+
     class Access_Modifiers
     {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
     }
 }
