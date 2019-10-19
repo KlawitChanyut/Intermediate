0a8173fe0bc89b02bb595c500937d0a69dcf5732
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 04:46:40 +0700

    Create class Interfaces_and_Extensibility

---
+++ Interfaces/Interfaces and Extensibility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Interfaces
+{
+    public class Interfaces_and_Extensibility
+    {
+        private readonly ILogger logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
