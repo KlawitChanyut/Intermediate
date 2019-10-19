e993917984505f037b69a639f2a1b2578b6c4f52
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 15:33:04 +0700

    Create class Fields

---
+++ Classes/Fields.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Classes
+{
+    public class Fields
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+
+        public Fields(int id)
+        {
+            this.Id = id;
+        }
+
+        public Fields(int id , string name)
+        {
+
+        }
+    }
+
+    public class Order
+    {
+
+    }
+}
