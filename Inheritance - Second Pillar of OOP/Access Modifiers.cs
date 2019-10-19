6e3b66fceb557d15736004228a0b5d4a20625a31
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 01:49:08 +0700

    Create class Access_Modifiers

---
+++ Inheritance - Second Pillar of OOP/Access Modifiers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Inheritance_Second
+{
+    public class Access_Modifiers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating();
+        }
+
+        public int CalculateRating()
+        {
+            return 0;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+        }
+    }
+}
+
