90cae500d4b83c62db0c2f345883dc4d74e38be5
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 02:07:27 +0700

    Create class Constructors_and_Inheritance

---
+++ Inheritance - Second Pillar of OOP/Constructors and Inheritance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Inheritance_Second
+{
+    public class Constructors_and_Inheritance
+    {
+        public Constructors_and_Inheritance()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+    }
+
+    public class Car : Constructors_and_Inheritance
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized.");
+        }
+    }
+}
