1a71af4cb7dafe1928d547a056322a2bba771806
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 04:21:41 +0700

    Create class Interfaces_and_Testability

---
+++ Interfaces/Interfaces and Testability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Interfaces
+{
+    public class Interfaces_and_Testability
+    {
+        static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor();
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
+        }
+    }
+}
