bf2019d66943085d56ed9e51c2f16cf75bf132ea
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 14:23:51 +0700

    Create class Constructors

---
+++ Classes/Constructors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Classes
+{
+    public class Constructors
+    {
+        public int Id;
+        public string Name;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
