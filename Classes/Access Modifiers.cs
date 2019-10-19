4de8d448a2f0ee53055e0be46a368bbc28b47675
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 15:46:01 +0700

    Create Access_Modifiers

---
+++ Classes/Access Modifiers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Classes
+{
+    public class Person
+    {
+        private DateTime _birtdate;
+    }
+    class Access_Modifiers
+    {
+    }
+}
