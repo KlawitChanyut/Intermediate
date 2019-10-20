834c90396e085b0fbc977d4636dc77fc09898a97
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 14:38:21 +0700

    Create class Stack_Exercuse

---
+++ Bonus Students Code Reviews/Stack Exercuse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Bonus_Students
+{
+    public class Stack_Exercuse
+    {
+        static void Main(string[] args)
+        {
+            Stack _stack = new Stack();
+
+            int ini = 0;
+            int man = 5;
+
+            for (int i = ini; i <= max; i++)
+                _stack.Push(i);
+
+            for (int i = ini; i <= max; i++)
+                Console.WriteLine(_stack.Pop());
+
+            Console.ReadLine();
+        }
+    }
+}
