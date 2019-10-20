a7972690534483912a88d39e1b2c06dcb5bae926
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 13:52:01 +0700

    Create class Stop_Watch_Exercise

---
+++ Bonus Students Code Reviews/Stop Watch Exercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.Bonus_Students
+{
+    public class Stop_Watch_Exercise
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start(DateTime.Now);
+            }
+        }
+    }
+}
