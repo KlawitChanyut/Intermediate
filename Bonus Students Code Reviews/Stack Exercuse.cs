274ed631e0809f02e3587946d85ce62a0ed567b3
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-20 14:46:56 +0700

    Update class Stack

--- Bonus Students Code Reviews/Stack Exercuse.cs
+++ Bonus Students Code Reviews/Stack Exercuse.cs
@@ -24,4 +24,28 @@ namespace Intermediate.Bonus_Students
             Console.ReadLine();
         }
     }
+
+    public class Stack
+    {
+        private List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You can not add a null object to the stack.");
+
+            _list.Add(obj);
+            int elements = _list.Count;
+
+            if (elements > 1)
+            {
+                for (int i = elements - 1; i > 0; i++)
+                {
+                    _list[i] = _list[i - 1];
+                }
+                _list[0] = obj;
+            }
+
+        }
+    }
 }
